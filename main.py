256a5fb89f7cfddee595f4540473845297f9a9f8
Diego <diegosantos4957@gmail.com>
2025-09-26 00:03:52 -0300

    feat: add OCR functionality to read text from an image

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import cv2
+import pytesseract
+
 def main():
+    img_cv = cv2.imread('test_image/images.png')
+    img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+    print(pytesseract.image_to_string(img_rgb))
     print("Hello, World!")
 
 
