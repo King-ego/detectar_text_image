52a8edca7b5fdb132ec142ed88402e1eeaff153c
Diego <diegosantos4957@gmail.com>
2025-09-26 21:13:11 -0300

    feat: remove "Hello, World!" print statement from main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def main():
     img_cv = cv2.imread('test_image/images.png')
     img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
     print(pytesseract.image_to_string(img_rgb))
-    print("Hello, World!")
 
 
 if __name__ == '__main__':
